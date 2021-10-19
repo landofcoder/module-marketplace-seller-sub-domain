62ad41f303e75999244c2356331a528a75d3d17a
landofcoder <landofcoder@gmail.com>
2021-10-19 18:41:47 +0700

    added module files

---
+++ Model/Seller.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Landofcoder
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Landofcoder.com license that is
+ * available through the world-wide-web at this URL:
+ * http://www.landofcoder.com/license-agreement.html
+ * 
+ * DISCLAIMER
+ * 
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ * 
+ * @category   Landofcoder
+ * @package    Lofmp_SubDomain
+ * @copyright  Copyright (c) 2017 Landofcoder (http://www.landofcoder.com/)
+ * @license    http://www.landofcoder.com/LICENSE-1.0.html
+ */
+namespace Lofmp\SubDomain\Model;
+
+use Magento\Framework\DataObject\IdentityInterface;
+
+/**
+ * Seller Model
+ */
+class Seller extends \Lof\MarketPlace\Model\Seller
+{	
+
+    public function getUrl()
+    {
+        if(!$this->_sellerHelper->getConfig('general_settings/enable_subdomain')) {
+            return;
+        }
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance ();
+        $seller_url = $objectManager->create('Lofmp\SubDomain\Model\Url');
+        $subdomain = $seller_url->load($this->getUrlKey(),'url_key');
+         
+        $url = $this->_storeManager->getStore()->getBaseUrl();
+        $route = $this->_sellerHelper->getConfig('general_settings/route');
+        $url_prefix = $this->_sellerHelper->getConfig('general_settings/url_prefix');
+        $urlPrefix = '';
+        if($url_prefix){
+            $urlPrefix = $url_prefix.'/';
+        }
+        $url_suffix = $this->_sellerHelper->getConfig('general_settings/url_suffix');
+      
+        if($subdomain->getData()) {
+            return $subdomain->getRequestPath();
+        }
+        return $url.$urlPrefix.$this->getUrlKey().$url_suffix;
+    }
+}
